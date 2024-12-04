b4f33abddadd7d17a6df971a04f9458eb12661c8
Igor Melnyk <igor_melnyk@ukr.net>
2024-12-04 23:56:11 +0200

    Update settings.sh

--- scripts/Extensions/Jackett/settings.sh
+++ scripts/Extensions/Jackett/settings.sh
@@ -1,1 +1,9 @@
-
+#!/bin/bash 
+if [[ -f "${DECKY_PLUGIN_RUNTIME_DIR}/conf_schemas/jacketttabconfig.json" ]]; then
+    TEMP="${DECKY_PLUGIN_RUNTIME_DIR}/conf_schemas/jacketttabconfig.json"
+else
+    TEMP="${DECKY_PLUGIN_DIR}/conf_schemas/jacketttabconfig.json"
+fi
+eval $(jq -r  '.Sections[] | "\(.Name | ascii_upcase)_\(.Options[0].Key | ascii_upcase)=\"\(.Options[0].Value)\""' $TEMP )
+eval $(jq -r  '.Sections[] | "\(.Name | ascii_upcase)_\(.Options[1].Key | ascii_upcase)=\"\(.Options[1].Value)\""' $TEMP )
+eval $(jq -r  '.Sections[] | "\(.Name | ascii_upcase)_\(.Options[2].Key | ascii_upcase)=\"\(.Options[2].Value)\""' $TEMP )
